3144da3c229a88dcd18f1f02c86372d0bc5e5076
Achmad Fawait <fawait626@gmail.com>
2024-12-12 10:50:16 +0700

    fix: test

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
+  console.log('oke')
 }
 bootstrap();
